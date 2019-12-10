c8977a6ebad28c57705c8f4d4cebc6c990bcf05e
V4lstein <valentin.cuteanu@gmail.com>
2019-12-11 01:26:14 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Resume from "./pages/Resume";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/portfolios" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const Resume = () => {
+  return <div>Resume</div>;
+};
+
+export default Resume;
