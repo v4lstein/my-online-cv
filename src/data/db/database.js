1a8094f5129f83adb43f5b0f36d3880bbdcdc5d3
V4lstein <valentin.cuteanu@gmail.com>
2019-12-11 19:21:29 +0200

    Add contact page

--- src/data/db/database.js
+++ src/data/db/database.js
@@ -2,9 +2,8 @@ import Mock from "../mock";
 
 const database = {
   information: {
-    name: "Valentin Cuteanu",
-    aboutContent:
-      "I am a web developer and guitar player from Cluj-Napoca, Romania. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.",
+    name: "Valentin",
+    aboutContent: "Web Developer -- Cluj-Napoca, Romania",
     age: 25,
     phone: "+40770636805",
     nationality: "Romanian",
@@ -13,18 +12,28 @@ const database = {
     address: "Cluj-Napoca",
     freelanceStatus: "Available",
     socialLinks: {
-      linkedin: "https://linkedin.com",
-      github: "https://github.com"
+      linkedin: "https://linkedin.com/in/valentin-cuteanu",
+      github: "https://github.com/v4lstein"
     },
     brandImage:
       "https://tctechcrunch2011.files.wordpress.com/2013/01/github-logo.png?w=300&h=300",
     aboutImage: "",
     aboutImageLg: "",
     cvfile: ""
+  },
+  contactInfo: {
+    phoneNumbers: ["+40770636805"],
+    emailAddress: ["valentin.cuteanu@gmail.com"],
+    address: "Cluj-Napoca, Romania"
   }
 };
 
 Mock.onGet("/api/information").reply(config => {
   const response = database.information;
+  return [200, response];
+});
+
+Mock.onGet("/api/contactinfo").reply(config => {
+  const response = database.contactInfo;
   return [200, response];
 });
