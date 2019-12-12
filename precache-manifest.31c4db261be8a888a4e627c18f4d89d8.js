ba11a29a7fb2829138cce345ebc0d4b82f2802fa
V4lstein <valentin.cuteanu@gmail.com>
2019-12-12 15:31:35 +0200

    Updates

---
+++ precache-manifest.31c4db261be8a888a4e627c18f4d89d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fed9a6dc113aeee7f6519ccbf285f64",
+    "url": "/my-online-cv/index.html"
+  },
+  {
+    "revision": "eb13d0ba357051249d77",
+    "url": "/my-online-cv/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "28335dd8d5c5b431f4d8",
+    "url": "/my-online-cv/static/css/main.dd90d70c.chunk.css"
+  },
+  {
+    "revision": "eb13d0ba357051249d77",
+    "url": "/my-online-cv/static/js/2.050470a5.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/my-online-cv/static/js/2.050470a5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28335dd8d5c5b431f4d8",
+    "url": "/my-online-cv/static/js/main.108a3edf.chunk.js"
+  },
+  {
+    "revision": "ced97e5b0fe9cc34fe1e",
+    "url": "/my-online-cv/static/js/runtime-main.dbddf7fb.js"
+  }
+]);
